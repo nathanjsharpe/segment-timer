eb0bc61bca57fc370360e6f7a06159ff33b33790
Nathan Sharpe <nathanjsharpe@gmail.com>
2016-12-04 22:21:20 -0500

    Add most basic multiple segments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import SegmentTimer from './SegmentTimer';
 import Segment from './Segment';
-import { testSegments as segments } from './segments';
+import segments from './segments';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSelectSegment = this.onSelectSegment.bind(this);
+
+    this.state = {};
+  }
+
+  onSelectSegment(e, currentSegment) {
+    e.preventDefault();
+    this.setState({ currentSegment })
+  }
+
   render() {
+    const { currentSegment } = this.state;
+
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Segment Timer</h2>
-        </div>
-        <SegmentTimer>
-          {segments.map((segment, index) => (
-            <Segment key={index} {...segment} />
+        <h2>Segment Timer</h2>
+        <p>Choose segment:</p>
+        <ul>
+          {Object.keys(segments).map(segment => (
+            <li>
+              <a href="#" onClick={e => this.onSelectSegment(e, segment)}>
+                {segment}
+              </a>
+            </li>
           ))}
-        </SegmentTimer>
+        </ul>
+
+        {currentSegment && (
+          <div className="content">
+            <h2>{currentSegment}</h2>
+            <SegmentTimer>
+              {segments[currentSegment].map((segment, index) => (
+                <Segment key={index} {...segment} />
+              ))}
+            </SegmentTimer>
+          </div>
+        )}
       </div>
     );
   }
